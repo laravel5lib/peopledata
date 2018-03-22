3ba59e32d8df024a37f6497e7d29ac3d2b339ea1
Juan Carlos Orrego <jcorrego@gmail.com>
2018-03-22 15:45:27 -0500

    Image fix

--- public/js/app.js
+++ public/js/app.js
@@ -67833,6 +67833,7 @@ __WEBPACK_IMPORTED_MODULE_0__node_modules_pnotify_dist_es_PNotify_js__["a" /* de
         this.on("error", function (file, errorMessage, xhr) {
           __WEBPACK_IMPORTED_MODULE_0__node_modules_pnotify_dist_es_PNotify_js__["a" /* default */].error("No se ha podido actualizar la imagen!");
           __WEBPACK_IMPORTED_MODULE_0__node_modules_pnotify_dist_es_PNotify_js__["a" /* default */].error(errorMessage);
+          __WEBPACK_IMPORTED_MODULE_0__node_modules_pnotify_dist_es_PNotify_js__["a" /* default */].error(xhr);
           window.vm.$children[0].image_layer = false;
         });
       }
