ff83002bb780b8cfb15b5fafdd8b20a0857576d9
Juan Carlos Orrego <jcorrego@gmail.com>
2018-06-27 01:42:20 -0500

    Simple blade template

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -10,52 +10,27 @@
             <div class="col-md">
                 <div class="card mb-2">
                     <div class="card-body">
-                        <a href="/courses/create" class="btn btn-primary">Agregar clase</a>        
+                        <a href="/courses/create" class="btn btn-primary">Agregar clase</a>
                     </div>
                 </div>
             </div>
         </div>
         <div class="card-group">
-            <div class="card">
-                <div class="card-header text-white bg-info">Lunes</div>
-                <div class="card-body">
-                    @foreach(\App\PCO\Course::where('day',1)->orderBy('hour')->get() as $course)
-                        @include('courses.summary')
-                    @endforeach
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header text-white bg-info">Martes</div>
-                <div class="card-body">
-                    @foreach(\App\PCO\Course::where('day',2)->get() as $course)
-                        @include('courses.summary')
-                    @endforeach
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header text-white bg-info">Miércoles</div>
-                <div class="card-body">
-                    @foreach(\App\PCO\Course::where('day',3)->get() as $course)
-                        @include('courses.summary')
-                    @endforeach
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header text-white bg-info">Jueves</div>
-                <div class="card-body">
-                    @foreach(\App\PCO\Course::where('day',4)->get() as $course)
-                        @include('courses.summary')
-                    @endforeach
-                </div>
-            </div>
-            <div class="card">
-                <div class="card-header text-white bg-info">Domingo</div>
-                <div class="card-body">
-                    @foreach(\App\PCO\Course::where('day',0)->get() as $course)
-                        @include('courses.summary')
-                    @endforeach
-                </div>
-            </div>
+            @php
+                $days = ['1'=>'Lunes','2'=>'Martes','3'=>'Miércoles','4'=>'Jueves','5'=>'Viernes','6'=>'Sábado','0'=>'Domingo'];
+            @endphp
+            @foreach($days as $index=>$day)
+                @if(($courses = \App\PCO\Course::where('day',$index)->orderBy('hour')->get()) && $courses->count())
+                    <div class="card">
+                        <div class="card-header text-white bg-info">{{ $day }}</div>
+                        <div class="card-body">
+                            @foreach($courses as $course)
+                                @include('courses.summary')
+                            @endforeach
+                        </div>
+                    </div>
+                @endif
+            @endforeach
         </div>
     </div>
 @endsection
