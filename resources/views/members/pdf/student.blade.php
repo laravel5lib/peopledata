15cc2de2ec3f379f3638d9b2306e3c96a4b366d7
Juan Carlos Orrego <jcorrego@gmail.com>
2019-05-20 15:32:07 -0500

    Period Line

--- resources/views/members/pdf/student.blade.php
+++ resources/views/members/pdf/student.blade.php
@@ -67,7 +67,7 @@
                             <td width="20%">
                                 <small style="color: #999;">Periodo:</small>
                                 <br>
-                                <span>{{ $course->period != '0-Anteriores'?$course->period:'__________________________' }}</span>
+                                <span>{{ $course->period != '0-Anteriores'?$course->period:'________________' }}</span>
                             </td>
                         </tr>
                     </table>
