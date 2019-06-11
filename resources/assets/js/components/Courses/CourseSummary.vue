9e42966ce40af1b26a9d215c97f0d57a3720acbb
Juan Carlos Orrego <jcorrego@gmail.com>
2019-06-11 09:14:14 -0500

    Hide 0 value

--- resources/assets/js/components/Courses/CourseSummary.vue
+++ resources/assets/js/components/Courses/CourseSummary.vue
@@ -2,7 +2,7 @@
     <div class="card mb-2 text-white cursor-pointer" :class="course.color" @click="gotoCourse(course.id)">
         <div class="card-body">
             <div class="float-right">
-                <span class="badge badge-light">${{ course.value }}</span><br>
+                <span class="badge badge-light" v-if="course.value">${{ course.value }}</span><br>
                 <span class="badge badge-dark" v-if="students">{{ students }} estudiantes</span>
             </div>
             <h5 class="card-title">{{ course.name }}</h5>
