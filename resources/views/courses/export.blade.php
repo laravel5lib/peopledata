74767211442730dc5883a48715b7d52a1c6c752b
Juan Carlos Orrego <jcorrego@gmail.com>
2019-10-28 08:21:57 -0500

    Export fix

--- resources/views/courses/export.blade.php
+++ resources/views/courses/export.blade.php
@@ -17,16 +17,16 @@
         @foreach($course->members as $member)
             @if( !count($status) || (count($status) && in_array($member->pivot->status, $status)) )
                 <tr>
-                    <td>{{ $course->name }}</td>
+{{--                    <td>{{ $course->name }}</td>--}}
 {{--                    <td>{{ ($course->day && isset($days[$course->day])) ? $days[$course->day] : '' }}</td>--}}
                     <td>-</td>
-                    <td>{{ $course->hour }}</td>
-                    <td>{{ optional($course->professor)->first_name . ' ' . optional($course->professor)->last_name }}</td>
-                    <td>{{ $member->first_name . ' ' . $member->last_name }}</td>
-                    <td>{{ $member->phone }}</td>
-                    <td>{{ $member->email }}</td>
-                    <td>{{ $member->pivot->payment }}</td>
-                    <td>{{ $options[$member->pivot->status] }}</td>
+{{--                    <td>{{ $course->hour }}</td>--}}
+{{--                    <td>{{ optional($course->professor)->first_name . ' ' . optional($course->professor)->last_name }}</td>--}}
+{{--                    <td>{{ $member->first_name . ' ' . $member->last_name }}</td>--}}
+{{--                    <td>{{ $member->phone }}</td>--}}
+{{--                    <td>{{ $member->email }}</td>--}}
+{{--                    <td>{{ $member->pivot->payment }}</td>--}}
+{{--                    <td>{{ $options[$member->pivot->status] }}</td>--}}
                 </tr>
             @endif
         @endforeach
